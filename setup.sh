01786ab8d187dafeeaee2897b2d1d9587d0fac87
tai <tai@www3324uf.sakura.ne.jp>
2014-03-25 12:25:48 +0900

    modify cp alias

--- setup.sh
+++ setup.sh
@@ -2,12 +2,13 @@
 # setup dotfiles
 
 THISDIR=`cd $(dirname $0);pwd`
-DOTFILES=(.bashrc .git .gitconfig .gitignore_global .vimrc .vim)
+DOTFILES=(.bashrc .gitignore_global .vimrc .vim)
 
 for FILE in ${DOTFILES[@]}
 do
-  ln -sfb ${THISDIR}/${FILE} ~/${FILE}
+  if [ -a ~/${FILE} ]; then
+    mv ~/${FILE} ~/${FILE}.bk
+  fi
+  ln -s ${THISDIR}/${FILE} ~/${FILE}
 done
-
-source ~/.bashrc
 
